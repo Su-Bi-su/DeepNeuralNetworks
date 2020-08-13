9d723b17db7f4d170baddfe31589004dbb8051b1
bhand <sushbhand123@gmail.com>
2020-08-13 17:05:41 +0200

    Updated Simple_CNN_MNIST.py

--- Simple_CNN_MNIST.py
+++ Simple_CNN_MNIST.py
@@ -1,4 +1,6 @@
 # Imports
+from abc import ABC
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -13,7 +15,7 @@ writer = SummaryWriter()
 
 # Design model (Network)
 
-class SimpleCNN(nn.Module):
+class SimpleCNN(nn.Module, ABC):
     def __init__(self, in_channels=1, num_class=10):
         super(SimpleCNN, self).__init__()
         self.conv1 = nn.Conv2d(in_channels=in_channels, out_channels=6, kernel_size=3, padding=1)
@@ -29,8 +31,8 @@ class SimpleCNN(nn.Module):
         x = F.relu(self.conv2(x))
         x = self.pool1(x)
         x = x.reshape(x.shape[0], -1)
-        x = self.fc1(x)
-        x = self.fc2(x)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
         x = self.fc3(x)
 
         return x
